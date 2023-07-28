28a8c84f5cf9c2a1376c6203e7cd3b44e44cc214
Amber Younus <amberyounus22@gmail.com>
2023-07-28 23:41:57 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState, useRef, useEffect } from 'react';
+import './App.css';
+
+// import axios from 'axios';
+// const options = {
+//   method: 'GET',
+//   url: 'https://quotes15.p.rapidapi.com/quotes/random/',
+//   headers: {
+//     'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+//     'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+//   }
+// };
+
+// try {
+// 	const response = await axios.request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+const App = () => {
+  const [quotes, setquotes] = useState('');
+  const textRef = useRef();
+  let colors = ["#2869d1", "#2869d1", "#a62b2b", "#1cb5bd", "#b7bf1f"]
+
+  const getQuote = () => {
+    fetch('https://type.fit/api/quotes')
+      .then((res) => res.json())
+      // .then(console.log);
+      .then((data) => {
+        let randomNum = Math.floor(Math.random() * data.length)
+        setquotes(data[randomNum])
+      })
+  }
+  useEffect(() => {
+    getQuote();
+  }, [])
+  useEffect(() => {
+    textRef.current.style.color = colors[Math.floor(Math.random() * colors.length)]
+  }, [quotes])
+
+  return (
+    <>
+      <div className="App">
+        <div className="quote">
+          <p ref={textRef}><b>{quotes.text}</b></p>
+          <br />
+          <p> Author : <i> {quotes.author}</i> </p>
+          <br />
+          <div className="btnContainer">
+            <button onClick={getQuote} className='btn'>Get Quote</button>
+            <a href={`https://twitter.com/intent/tweet?text=${quotes.text}`}
+              target="_blank"
+              rel='noreferrer noopener'
+              className='btn'>Tweet</a>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
